c2d9db44f526f8ca6d321f426e265d43d1ae6c3d
Bryan Urizar <bryan.urizar@outlook.com>
2021-01-05 02:16:38 +0000

    small changes

--- public/javascript/client.js
+++ public/javascript/client.js
@@ -1,5 +1,11 @@
 'use strict';
 
+document.addEventListener('click', titleClick);
+
+function titleClick() {
+    alert('title was clicked');
+}
+
 let todoItems = document.getElementsByClassName('todo-card');
 
 for (let i = 0; i < todoItems.length; i++) {
@@ -7,6 +13,7 @@ for (let i = 0; i < todoItems.length; i++) {
 }
 
 function handleClick(e) {
+    alert('card was clicked');
     const todoTag = e.target.closest('.todo-card');
     const todoDescription = todoTag.getElementsByClassName('todo-description')[0].innerText;
     const todoCheckbox = todoTag.querySelector('input[type="checkbox"]');
