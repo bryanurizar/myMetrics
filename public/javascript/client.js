6715043d81a29a7925b6e1c5746a907fe9560a43
Bryan Urizar <bryan.urizar@outlook.com>
2021-02-11 02:47:40 +0000

    HTML rendered for the List class and I have created the ListItem class to create objects for each item

--- public/javascript/client.js
+++ public/javascript/client.js
@@ -93,57 +93,49 @@ function handleClick(e) {
     }
 }
 
-/*
-***
-*** Drag and Drop API Implementation
-***
-*/
+// /*
+// ***
+// *** Drag and Drop API Implementation
+// ***
+// */
 
-const draggableCards = document.querySelectorAll('.todo-card');
-const dropZone = document.querySelector('.todos');
+// const draggableCards = document.querySelectorAll('.todo-card');
+// const dropZone = document.querySelector('.todos');
 
-const handleDragStart = e => {
-    e.dataTransfer.setData('text/plain', e.target.id);
-    e.dataTransfer.dropEffect = 'move';
-};
+// const handleDragStart = e => {
+//     e.dataTransfer.setData('text/plain', e.target.id);
+//     e.dataTransfer.dropEffect = 'move';
+// };
 
-const handleDragOver = e => {
-    e.preventDefault();
-    e.dataTransfer.dropEffect = 'move';
-};
+// const handleDragOver = e => {
+//     e.preventDefault();
+//     e.dataTransfer.dropEffect = 'move';
+// };
 
-const handleDrop = e => {
-    e.preventDefault();
-    const cardId = e.dataTransfer.getData('text/plain');
-    const dropNode = e.target.closest('.todo-card');
+// const handleDrop = e => {
+//     e.preventDefault();
+//     const cardId = e.dataTransfer.getData('text/plain');
+//     const dropNode = e.target.closest('.todo-card');
 
-    if (isMouseAboveMiddle(e)) {
-        dropNode.insertAdjacentElement('beforebegin', document.querySelector(`#${cardId}`));
-    } else {
-        dropNode.insertAdjacentElement('afterend', document.querySelector(`#${cardId}`));
-    }
-};
+//     if (isMouseAboveMiddle(e)) {
+//         dropNode.insertAdjacentElement('beforebegin', document.querySelector(`#${cardId}`));
+//     } else {
+//         dropNode.insertAdjacentElement('afterend', document.querySelector(`#${cardId}`));
+//     }
+// };
 
-draggableCards.forEach(draggableCard => {
-    draggableCard.addEventListener('dragstart', handleDragStart);
-});
+// draggableCards.forEach(draggableCard => {
+//     draggableCard.addEventListener('dragstart', handleDragStart);
+// });
 
-function isMouseAboveMiddle(e) {
-    const bounds = e.target.getBoundingClientRect();
-    const y = e.clientY - bounds.top;
-    return y < bounds.height / 2;
-}
+// function isMouseAboveMiddle(e) {
+//     const bounds = e.target.getBoundingClientRect();
+//     const y = e.clientY - bounds.top;
+//     return y < bounds.height / 2;
+// }
 
-dropZone.addEventListener('drop', handleDrop);
-dropZone.addEventListener('dragover', handleDragOver);
-
-// Add new list functionality
-const newListElement = document.querySelector('.add-list');
-newListElement.addEventListener('click', handleNewList);
-
-function handleNewList() {
-
-}
+// dropZone.addEventListener('drop', handleDrop);
+// dropZone.addEventListener('dragover', handleDragOver);
 
 function handleModal(e) {
     const modalId = `#modal-${e.target.id}`;
@@ -248,41 +240,62 @@ function handleCreateTargetListClick() {
 (async () => {
     const response = await fetch('http://localhost:3000/getTodos');
     const todos = await response.text();
-    console.log(todos);
+    // console.log(todos);
 
 })();
 
 class List {
-    constructor() {
-
+    constructor(id, listName, ...listItems) {
+        this.id = id;
+        this.listName = listName;
+        this.todos = [...listItems];
     }
     render() {
         const list = document.createElement('div');
         list.classList.add('todo-list-container');
-        list.innerHTML = document.createElement('div');
 
+        const header = document.createElement('div');
+        header.classList.add('list-header');
+        list.appendChild(header);
 
+        const listTitle = document.createElement('h4');
+        listTitle.classList.add('list-name');
+        listTitle.contentEditable = true;
+        listTitle.innerText = this.listName;
+        header.appendChild(listTitle);
+
+        const listModal = document.createElement('h4');
+        listModal.id = this.id;
+        listModal.classList.add('list-popup');
+        listModal.innerText = '...';
+        header.appendChild(listModal);
+
+        const modal = document.createElement('div');
+        modal.innerHTML =
+            `<div id="${this.id}" class="modal">
+                <h4 id="modal-title"><span>List Actions</span></h4>
+                <p id="${this.id}" class="delete-list">Delete This List..</p>
+            </div>`;
+        header.appendChild(modal);
 
         const board = document.querySelector('#board');
-
-        board.insertAdjacentElement('afterbegin', list);
-
+        board.insertAdjacentElement('beforeend', list);
     }
 }
 
-const list = new List();
-list.render();
+const list1 = new List(1, 'UI/UX Features', {});
+list1.render();
 
-
+const list2 = new List(2, 'Reading List', 'hello');
+list2.render();
 
 class ListItem {
-    constructor() {
-        // What do I really want here?
-        // How do I get data from the backend?
-        // I want it to be able to feed in the cards?
-        // Do I use the rest parameters to allow any number of arguments?
-        // But how do I feed them in and collect the ones specifically for each card?
-        // Request for each card or reques tall the cards and then process them in the frontend?
+    constructor(listId, itemId, itemDescription) {
+        this.listId = listId;
+        this.itemId = itemId;
+        this.itemDescription = itemDescription;
     }
-    render() { }
 }
+
+const item = new ListItem(2, 251, 'Read JavaScript: The Definitive Guide');
+console.log(item);
