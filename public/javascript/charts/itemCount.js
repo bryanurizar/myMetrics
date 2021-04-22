d84adf712ae3465c3158163054a4d3cb4396231f
Bryan Urizar <bryan.urizar@outlook.com>
2021-04-22 03:59:17 +0100

    api request for chart working now

--- public/javascript/charts/itemCount.js
+++ public/javascript/charts/itemCount.js
@@ -1,58 +1,48 @@
 async function getChartData() {
-    const response = await fetch('http://localhost:3000/items', {
+    const response = await fetch('http://localhost:3000/data', {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
         }
     });
-    return response.json();
-}
+    const res = await response.json();
 
-console.log(getChartData());
+    const labels = res.boardNames;
+    const data = {
+        labels: labels,
+        datasets: [{
+            label: 'Number of Items',
+            backgroundColor: 'rgb(48, 111, 216)',
+            borderColor: 'rgb(48,111,216)',
+            data: res.itemCount,
+        }]
+    };
 
-
-
-
-
-
-
-const labels = [
-    'JavaScript',
-    'myMetrics App',
-    'Random Bits'
-];
-const data = {
-    labels: labels,
-    datasets: [{
-        label: 'Number of Items',
-        backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
-            'rgba(255, 205, 86, 0.2)'
-        ],
-        borderColor: [
-            'rgb(255, 99, 132)',
-            'rgb(255, 159, 64)',
-            'rgb(255, 205, 86)',
-        ],
-        data: [4, 16, 1],
-    }]
-};
-
-const config = {
-    type: 'bar',
-    data,
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
+    const config = {
+        type: 'bar',
+        data,
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
             }
         }
-    }
-};
+    };
 
-// === include 'setup' then 'config' above ===
-const myChart = new Chart(
-    document.querySelector('#item-count'),
-    config
-);
+    const myChart = new Chart(
+        document.querySelector('#item-count'),
+        config
+    );
+}
+
+getChartData();
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
