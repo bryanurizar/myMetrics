91f2604f6a2609f858d28c261cef277566981e02
Bryan Urizar <bryan.urizar@outlook.com>
2021-05-05 00:02:32 +0100

    began working on study time page

--- public/javascript/study-time.js
+++ public/javascript/study-time.js
@@ -1,6 +1,42 @@
 'use strict';
 
+import Duration from 'luxon/src/duration.js';
+
 const studySection = document.querySelector('#study-timer');
+
 const timer = document.createElement('div');
-timer.innerText = 'Hello there';
 studySection.appendChild(timer);
+
+const hoursInput = document.createElement('input');
+const minutesInput = document.createElement('input');
+const startButton = document.createElement('button');
+
+startButton.innerText = 'Start';
+
+timer.appendChild(hoursInput);
+timer.appendChild(minutesInput);
+timer.appendChild(startButton);
+
+startButton.addEventListener('click', startButtonClick);
+
+function startButtonClick() {
+
+    const studyTime = {
+        hours: Number(hoursInput.value),
+        minutes: Number(minutesInput.value)
+    };
+    postStudyTime(studyTime);
+    console.log(studyTime);
+}
+
+async function postStudyTime(studyTime) {
+    const response = await fetch('http://localhost:3000/start-time', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(studyTime)
+    });
+
+    return response.text;
+}
