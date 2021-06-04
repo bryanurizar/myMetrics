e8727853382fa7a2904a84af946e24db31965804
Bryan Urizar <bryan.urizar@outlook.com>
2021-06-04 02:39:15 +0100

    added some timer code that i need to review

--- public/javascript/study-session.js
+++ public/javascript/study-session.js
@@ -1,13 +1,13 @@
 import * as luxon from 'https://moment.github.io/luxon/es6/luxon.min.js';
 const Duration = luxon.Duration;
 
-let studySessionDuration = Duration.fromObject({ hours: 10, minutes: 0, seconds: 0 });
-setInterval(countdown, 1000);
+// let studySessionDuration = Duration.fromObject({ hours: 10, minutes: 0, seconds: 0 });
+// setInterval(countdown, 1000);
 
-function countdown() {
-    studySessionDuration = studySessionDuration.minus({ seconds: 1 });
-    console.log(studySessionDuration.toFormat('hh : mm : ss'));
-}
+// function countdown() {
+//     studySessionDuration = studySessionDuration.minus({ seconds: 1 });
+//     console.log(studySessionDuration.toFormat('hh : mm : ss'));
+// }
 
 const studySection = document.querySelector('#study-timer');
 
@@ -72,3 +72,33 @@ function createTimer(sessionTime) {
     cancelButton.innerText = 'Cancel';
     studySection.appendChild(cancelButton);
 }
+
+
+// Set the date we're counting down to
+var countDownDate = new Date('Jan 5, 2022 15:37:25').getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    document.getElementById('demo').innerHTML = days + 'd ' + hours + 'h '
+        + minutes + 'm ' + seconds + 's ';
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById('demo').innerHTML = 'EXPIRED';
+    }
+}, 1000);
