30676e6fb903b6a33f87088f619ffe7447ee74c6
Bryan Urizar <bryan.urizar@outlook.com>
2021-06-14 01:16:32 +0100

    timer rendered

--- public/javascript/study-session.js
+++ public/javascript/study-session.js
@@ -1,14 +1,6 @@
 import * as luxon from 'https://moment.github.io/luxon/es6/luxon.min.js';
 const Duration = luxon.Duration;
 
-let studySessionDuration = Duration.fromObject({ hours: 10, minutes: 0, seconds: 0 });
-setInterval(countdown, 1000);
-
-function countdown() {
-    studySessionDuration = studySessionDuration.minus({ seconds: 1 });
-    console.log(studySessionDuration.toFormat('hh : mm : ss'));
-}
-
 const studySection = document.querySelector('#study-timer');
 
 const timer = document.createElement('div');
@@ -25,9 +17,9 @@ timer.appendChild(hoursInput);
 timer.appendChild(minutesInput);
 timer.appendChild(startButton);
 
-startButton.addEventListener('click', startButtonClick);
+startButton.addEventListener('click', startTimer);
 
-function startButtonClick() {
+function startTimer() {
 
     const sessionUrl = new URL(window.location.href);
     const sessionId = sessionUrl.pathname.split('/')[2];
@@ -54,15 +46,21 @@ async function postStudyTime(sessionData) {
         },
         body: JSON.stringify(sessionData)
     });
-
-    return response.text;
+    return response;
 }
 
 function createTimer(sessionTime) {
     timer.remove();
     const countdownTimer = document.createElement('div');
-    countdownTimer.innerHTML = `<p>${sessionTime.hours} : ${sessionTime.minutes}</p>`;
-    studySection.appendChild(countdownTimer);
+
+    let studySessionDuration = Duration.fromObject({ hours: sessionTime.hours, minutes: sessionTime.minutes, seconds: sessionTime.seconds });
+    setInterval(countdown, 1000);
+
+    function countdown() {
+        studySessionDuration = studySessionDuration.minus({ seconds: 1 });
+        countdownTimer.innerHTML = studySessionDuration.toFormat('hh : mm : ss');
+        studySection.appendChild(countdownTimer);
+    }
 
     const pauseButton = document.createElement('button');
     pauseButton.innerText = 'Pause';
@@ -73,32 +71,14 @@ function createTimer(sessionTime) {
     studySection.appendChild(cancelButton);
 }
 
+function resumeTimer() {
 
-// Set the date we're counting down to
-var countDownDate = new Date('Jan 5, 2022 15:37:25').getTime();
+}
 
-// Update the count down every 1 second
-var x = setInterval(function () {
+function pauseTimer() {
 
-    // Get today's date and time
-    var now = new Date().getTime();
+}
 
-    // Find the distance between now and the count down date
-    var distance = countDownDate - now;
+function cancelTimer() {
 
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    // Display the result in the element with id="demo"
-    document.getElementById('demo').innerHTML = days + 'd ' + hours + 'h '
-        + minutes + 'm ' + seconds + 's ';
-
-    // If the count down is finished, write some text
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById('demo').innerHTML = 'EXPIRED';
-    }
-}, 1000);
+}
