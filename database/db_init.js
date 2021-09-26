8c04a88c60ea555506e453dd24ae0e5aff260a9e
Bryan Urizar <bryan.urizar@outlook.com>
2021-09-26 02:44:12 +0100

    fix in db configuration

--- database/db_init.js
+++ database/db_init.js
@@ -5,7 +5,7 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const pool = new Pool({
-    user: process.env.DB_NAME,
+    user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
